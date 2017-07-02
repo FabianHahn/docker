9c9575fd98857299f460578726544d746533576f
Fabian Hahn <fabian@hahn.graphics>
2017-07-02 16:47:37 +0000

    write letsencrypt renew output to log file

--- nginx-proxy/letsencrypt-renew.sh
+++ nginx-proxy/letsencrypt-renew.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 letsencrypt renew \
 	--text --non-interactive --agree-tos \
-	--email fabian@hahn.graphics
+	--email fabian@hahn.graphics \
+	2>&1 >>/var/log/letsencrypt-renew.log
 service nginx reload
